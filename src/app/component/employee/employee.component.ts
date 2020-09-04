4d763cf2f3aeea39ac458e2df7fbbbeffbccd2e5
Nomaan Shaikh <shaikh.nomaan67@gmail.com>
2020-09-04 18:29:50 +0530

    integrated cross expirence filter

--- src/app/component/employee/employee.component.ts
+++ src/app/component/employee/employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { candidate_data } from '../../data/candidates-data';
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-employee',
@@ -12,6 +13,8 @@ export class EmployeeComponent implements OnInit {
   employee_list = candidate_data;
   department_list = [];
   select_department = "";
+  select_experience = "";
+  search_employee = "";
 
   constructor() { }
 
@@ -27,7 +30,6 @@ export class EmployeeComponent implements OnInit {
   }
 
   filterEmployeeListByDepartrment(department){
-    console.log(department);
     if(department != "Select"){
       let arr = candidate_data.slice(0);
       let filtered_department = arr.filter(function(emp) {
@@ -39,6 +41,66 @@ export class EmployeeComponent implements OnInit {
       this.employee_list = candidate_data;
     }
   }
+
+  filterEmployeeListByExperience(experience){
+    console.log(this.select_department);
     
+    console.log("filterEmployeeListByExperience",experience);
+    let arr = candidate_data.slice(0);
+    if(experience != "Select"){
+      let filtered_department = arr.filter(function(emp) {
+        let a = moment( [new Date(emp.joining_date).getFullYear(), new Date(emp.joining_date).getMonth()]);
+        let b = moment( [new Date().getFullYear(), new Date().getMonth()]);
+        // console.log((b.diff(a,"years")),emp.name);
+        return (b.diff(a,"years")) > experience; 
+      }.bind(this));
+
+      if(this.select_department && this.select_department.department != "Select"){ //if department filter is applied
+        let temp = filtered_department.filter(function(emp) {
+          return emp.department == this.select_department.department; 
+        }.bind(this));
+
+        this.employee_list = (temp).slice(0);
+      }else{
+        this.employee_list = (filtered_department).slice(0);
+      }
+    }else{  //if not any experience filter applied
+      if(this.select_department && this.select_department.department != "Select"){   //if department filter is applied
+        let temp = arr.filter(function(emp) {
+          return emp.department == this.select_department.department; 
+        }.bind(this));
+        
+        this.employee_list = (temp).slice(0);
+      }else{  //if not any department filter is applied
+        console.log(candidate_data);
+        
+        this.employee_list = candidate_data;
+      }
+    }
+  }
+    
+  diffYears(dt2, dt1){
+    var diff = ((dt2) - (dt1)) / 1000;
+    diff /= (60 * 60 * 24);
+    return Math.abs(Math.round(diff/365.25));
+  }
+
+    searchEmployeeName(search_employee) {
+      console.log(search_employee);
+      
+      let arr = candidate_data.slice(0);
+      if (!search_employee.trim().length) {
+        this.employee_list = arr;
+      } else {
+        this.employee_list = arr.filter(emp => {
+        var words = _.words(emp.name)
+        .join(" ")
+        .toLowerCase();
+        words = words + " " + emp.name.toLowerCase();
+        var isSubstring = words.indexOf(search_employee.toLowerCase()) !== -1;
+        return isSubstring;
+      });
+      }
+  }
 
 }
