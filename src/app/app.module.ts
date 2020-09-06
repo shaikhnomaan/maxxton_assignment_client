8510555d04764aec6e359dabf7c9dad1a6d356a3
Nomaan Shaikh <shaikh.nomaan67@gmail.com>
2020-09-06 13:37:48 +0530

    remove employee from employee list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './component/employee/employee.component';
+
 import { FormsModule } from '@angular/forms';
-
 import { FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core'; 
-import { faCoffee  } from '@fortawesome/free-solid-svg-icons';
-library.add(faCoffee);
-
 
 
 @NgModule({
